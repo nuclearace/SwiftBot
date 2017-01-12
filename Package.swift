77b38690c59d23ef16320c719590ca94e14e3587
Erik Little <nuclear.ace@gmail.com>
2017-01-12 06:48:51 -0500

    bump swiftdiscord version

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftBot",
     dependencies: [
-        .Package(url: "https://github.com/nuclearace/SwiftDiscord", majorVersion: 1),
+        .Package(url: "https://github.com/nuclearace/SwiftDiscord", majorVersion: 2),
         .Package(url: "https://github.com/nuclearace/ImageBrutalizer", majorVersion: 1),
         .Package(url: "https://github.com/nuclearace/SwiftRateLimiter", majorVersion: 1)
     ]
